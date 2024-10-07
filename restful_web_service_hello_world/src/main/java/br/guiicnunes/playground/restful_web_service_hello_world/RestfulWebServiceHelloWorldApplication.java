c13c93cbaa81dbb88d88fce7c820d73f041feb30
Guilherme Nunes <guiicnunes@gmail.com>
2024-10-07 20:30:58 -0300

    chore: initializes spring project

---
+++ restful_web_service_hello_world/src/main/java/br/guiicnunes/playground/restful_web_service_hello_world/RestfulWebServiceHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package br.guiicnunes.playground.restful_web_service_hello_world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebServiceHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebServiceHelloWorldApplication.class, args);
+	}
+
+}
---
+++ restful_web_service_hello_world/src/test/java/br/guiicnunes/playground/restful_web_service_hello_world/RestfulWebServiceHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package br.guiicnunes.playground.restful_web_service_hello_world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServiceHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
