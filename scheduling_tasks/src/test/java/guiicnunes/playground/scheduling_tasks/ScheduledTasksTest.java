c280017582c8bfc9966d4194013ae07e6f201754
Guilherme Nunes <guiicnunes@gmail.com>
2024-10-09 09:01:18 -0300

    feat: tests functions

---
+++ scheduling_tasks/src/test/java/guiicnunes/playground/scheduling_tasks/ScheduledTasksTest.java
@@ -0,0 +1,33 @@
+package guiicnunes.playground.scheduling_tasks;
+
+import org.awaitility.Durations;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+
+import java.time.Duration;
+
+import static org.awaitility.Awaitility.await;
+import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest
+public class ScheduledTasksTest {
+
+    @SpyBean
+    ScheduledTasks tasks;
+
+    @Test
+    public void reportCurrentTime() {
+        await().atMost(Durations.TEN_SECONDS).untilAsserted(() -> {
+            verify(tasks, atLeast(2)).reportCurrentTime();
+        });
+    }
+
+    @Test
+    public void alarm() {
+        await().atMost(Duration.ofMinutes(3)).untilAsserted(() -> {
+            verify(tasks, atLeast(1)).alarm();
+        });
+    }
+}
