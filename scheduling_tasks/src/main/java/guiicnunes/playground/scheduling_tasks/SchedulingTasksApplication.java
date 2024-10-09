e12dd083443cc554903804806aecff679ade02bd
Guilherme Nunes <guiicnunes@gmail.com>
2024-10-09 09:00:10 -0300

    feat: enables scheduling annotation

--- scheduling_tasks/src/main/java/guiicnunes/playground/scheduling_tasks/SchedulingTasksApplication.java
+++ scheduling_tasks/src/main/java/guiicnunes/playground/scheduling_tasks/SchedulingTasksApplication.java
@@ -2,8 +2,10 @@ package guiicnunes.playground.scheduling_tasks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SchedulingTasksApplication {
 
 	public static void main(String[] args) {
